32395461e2220f699334b7e6c6b64c6ea18c6cf0
Andrei Miashkou <meshkow.andrei@gmail.com>
2020-06-21 17:26:12 +0300

    feat: add new kata

---
+++ 7kyu/7kyu-regexp-basics-is-it-a-letter.js
@@ -0,0 +1,7 @@
+// Regexp Basics - is it a letter?
+
+// Complete the code which should return true if the given object is a single ASCII letter (lower or upper case), false otherwise.
+
+String.prototype.isLetter = function () {
+	return /^[A-Za-z]$/.test(this)
+}
