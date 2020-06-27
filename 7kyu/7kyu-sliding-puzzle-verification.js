65e73d62f3a7e5a04b2abb4c1e294b7f05552af8
Andrei Miashkou <meshkow.andrei@gmail.com>
2020-06-27 17:19:28 +0300

    feat: add new katas

---
+++ 7kyu/7kyu-sliding-puzzle-verification.js
@@ -0,0 +1,32 @@
+// Sliding Puzzle Verification
+
+// Given a board of NxN, distributed with tiles labeled 0 to N² - 1(inclusive):
+
+// A solved grid will have the tiles in order of label, left to right, top to bottom.
+
+// Return true if the board state is currently solved, and false if the board state is unsolved.
+
+// Input will always be a square 2d array.
+
+// For example, a 2x2 solved grid:
+
+// [ [0, 1],
+//   [2, 3] ]
+// A 2x2 unsolved grid:
+
+// [ [2, 1],
+//   [0, 3] ]
+
+function isSolved(board) {
+	const n = board.length;
+	let counter = 0;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			if (board[i][j] !== counter) {
+				return false;
+			}
+			counter++;
+		}
+	}
+	return true;
+}
