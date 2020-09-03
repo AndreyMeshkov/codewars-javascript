3f0552d79169f8e206820e2effd5645c771a8e8c
Andrei Miashkou <meshkow.andrei@gmail.com>
2020-09-03 19:29:56 +0300

    fead: add katas

---
+++ beta/beta-a-function-within-a-function.js
@@ -0,0 +1,10 @@
+// A function within a function
+
+// 	Given an input n, write a function always that returns a function which returns n.Ruby should return a lambda or a proc.
+
+// var three = always(3);
+// three(); // returns 3
+
+function always(n) {
+	return () => n;
+}
