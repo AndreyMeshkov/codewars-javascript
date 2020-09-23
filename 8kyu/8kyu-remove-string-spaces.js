82215508bd70e8b42d93d94881eb7622f678583d
Andrei Miashkou <meshkow.andrei@gmail.com>
2020-09-23 15:04:06 +0300

    feat: add katas

---
+++ 8kyu/8kyu-remove-string-spaces.js
@@ -0,0 +1,13 @@
+// Remove String Spaces
+
+// Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x) {
+	return x.split(' ').join('');
+}
+
+// Best Practices:
+
+// function noSpace(x) {
+// 	return x.replace(/\s/g, '');
+// }
