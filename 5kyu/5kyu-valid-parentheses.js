6b359a841645a91cd72f3251218c9d3f5e1b6904
Andrei Miashkou <meshkow.andrei@gmail.com>
2020-09-24 14:25:29 +0300

    feat: add katas

--- 5kyu/5kyu-valid-parentheses.js
+++ 5kyu/5kyu-valid-parentheses.js
@@ -11,5 +11,25 @@
 // 0 <= input.length <= 100
 
 function validParentheses(parens) {
-	arr = parens.split('')
+	const arr = parens.split('');
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i + 1] === ')' && arr[i] === '(') {
+			arr.splice(i, 2);
+			i -= 2;
+		}
+	}
+	return !arr.length;
 }
+
+// Best Practices:
+
+// function validParentheses(parens) {
+// 	var n = 0;
+// 	for (var i = 0; i < parens.length; i++) {
+// 		if (parens[i] == '(') n++;
+// 		if (parens[i] == ')') n--;
+// 		if (n < 0) return false;
+// 	}
+
+// 	return n == 0;
+// }
