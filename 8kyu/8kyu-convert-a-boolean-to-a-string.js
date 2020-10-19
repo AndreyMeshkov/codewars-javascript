4a3c1aef0a7628b2fb5e38791b9cac654fbfcf1c
Andrei Miashkou <meshkow.andrei@gmail.com>
2020-10-19 13:56:26 +0300

    feat: add katas

---
+++ 8kyu/8kyu-convert-a-boolean-to-a-string.js
@@ -0,0 +1,15 @@
+// Convert a Boolean to a String
+
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd
+
+// Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b){
+  return String(b);
+}
+
+// Best Practices and clever solution:
+
+// function booleanToString(b){
+//   return b.toString();
+// }
