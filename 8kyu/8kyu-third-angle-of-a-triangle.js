c09cecaeb2abfd149223654495ad5266e2485a7d
Andrei Miashkou <meshkow.andrei@gmail.com>
2020-11-05 15:30:43 +0300

    feat: add katas

---
+++ 8kyu/8kyu-third-angle-of-a-triangle.js
@@ -0,0 +1,25 @@
+// Third Angle of a Triangle
+
+// https://www.codewars.com/kata/5a023c426975981341000014
+
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+// https://en.wikipedia.org/wiki/Triangle
+
+function otherAngle(a, b) {
+    return 180 - a - b;
+}
+
+// Best Practices:
+
+// function otherAngle(a, b) {
+//   return 180-(a+b);
+// }
+
+// Clever solution:
+
+// const otherAngle = (a, b) => 180 - a - b
