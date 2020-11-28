1a5a63c40f79ff67f12d701d0bf7d4b4b19bc58e
Andrei Miashkou <meshkow.andrei@gmail.com>
2020-11-28 16:15:54 +0300

    feat: add katas

---
+++ 8kyu/8kyu- Grasshopper -function-syntax-debugging.js
@@ -0,0 +1,21 @@
+// Grasshopper - Function syntax debugging
+
+// https://www.codewars.com/kata/56dae9dc54c0acd29d00109a/train/javascript
+
+// Grasshopper - Function syntax debugging
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+function main(verb, noun) {
+  return `${verb}${noun}`;
+}
+
+// Best practices:
+
+// function main (verb, noun) {
+//   return verb + noun
+// }
+
+// Clever solution:
+
+// const main = String.prototype.concat.bind('');
+
