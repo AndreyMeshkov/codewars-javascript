1a8876bba4b0752b240294989ff9f70215e6acbd
Andrei Miashkou <meshkow.andrei@gmail.com>
2020-12-12 12:58:53 +0300

    feat: add katas

---
+++ 8kyu/8kyu-function-3-multiplying-two-numbers.js
@@ -0,0 +1,16 @@
+// Function 3 - multiplying two numbers
+
+// https://www.codewars.com/kata/523b66342d0c301ae400003b
+
+// Implement a function which multiplies two numbers.
+
+const multiply = (a, b) => {
+  return a * b;
+}
+
+// Best practice and clever solution:
+
+// function multiply(a, b){
+//   if(typeof a == 'number' && typeof b == 'number')
+//     return a * b;  
+// }
