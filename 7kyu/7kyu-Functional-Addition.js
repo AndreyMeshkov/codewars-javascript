94eb5e26e1921c5c7272a0a0dade47ddde8e1f31
Andrei Miashkou <meshkow.andrei@gmail.com>
2021-03-19 10:16:34 +0300

    feat: add katas

---
+++ 7kyu/7kyu-Functional-Addition.js
@@ -0,0 +1,27 @@
+// Functional Addition
+
+// https://www.codewars.com/kata/538835ae443aae6e03000547
+
+// Create a function add(n)/Add(n) which returns a function that always adds n to any number
+
+// Note for Java: the return type and methods have not been provided to make it a bit more challenging.
+
+// var addOne = add(1);
+// addOne(3); // 4
+
+// var addThree = add(3);
+// addThree(3); // 6
+
+function add(n) {
+  return function (num) {
+    return n + num;
+  };
+}
+
+// Best practice and clever solution:
+
+// function add(n) {
+//     return function (m) {
+//       return n+m;
+//     }
+//   }
