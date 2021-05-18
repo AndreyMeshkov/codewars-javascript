fdd955172b8e34b03c419ceece988e4f9dbb37b6
Andrei Miashkou <meshkow.andrei@gmail.com>
2021-05-18 20:40:28 +0300

    feat: add katas

---
+++ 7kyu/7kyu-Last-Survivor.js
@@ -0,0 +1,44 @@
+// Last Survivor
+
+// https://www.codewars.com/kata/609eee71109f860006c377d1
+
+// You are given a string of letters and an array of numbers.
+// The numbers indicate positions of letters that must be removed, in order, starting from the beginning of the array.
+// After each removal the size of the string decreases (there is no empty space).
+// Return the only letter left.
+
+// Example:
+
+// let str = "zbk", arr = [0, 1]
+//     str = "bk", arr = [1]
+//     str = "b", arr = []
+//     return 'b'
+// Notes
+// The given string will never be empty.
+// The length of the array is always one less than the length of the string.
+// All numbers are valid.
+// There can be duplicate letters and numbers.
+// If you like this kata, check out the next one: Last Survivors Ep.2
+
+function lastSurvivor(letters, coords) {
+  let arr = letters.split("");
+  while (coords.length > 0) {
+    arr.splice(coords[0], 1);
+    coords.shift();
+  }
+  return arr.join("");
+}
+
+// Best practice:
+
+// function lastSurvivor(letters, coords) {
+//   letters = letters.split('');
+//   for (let i = 0; i < coords.length; i++) {
+//      letters.splice(coords[i], 1)
+//   }
+//   return letters.join('')
+// }
+
+// Clever solution:
+
+// function lastSurvivor(letters,coords) { return coords.reduce( (letters,coord) => letters.slice(0,coord) + letters.slice(coord+1) , letters ); }
---
+++ 8kyu/8kyu-What's-up-next.js
@@ -0,0 +1,36 @@
+// What's up next?
+
+// https://www.codewars.com/kata/542ebbdb494db239f8000046
+
+// Given a sequence of items and a specific item in that sequence, return the item immediately following the item specified. If the item occurs more than once in a sequence, return the item after the first occurence. This should work for a sequence of any type.
+
+// When the item isn't present or nothing follows it, the function should return nil in Clojure and Elixir, Nothing in Haskell, undefined in JavaScript, None in Python.
+
+// nextItem([1, 2, 3, 4, 5, 6, 7], 3) # 4
+// nextItem("testing", "t") # "e"
+
+function nextItem(xs, item) {
+  let end = false;
+  let result;
+  for (let i of xs) {
+    if (end) {
+      result = i;
+      break;
+    }
+    if (i === item) {
+      end = true;
+    }
+  }
+  return result;
+}
+
+// Best practice and clever solution:
+
+// function nextItem(xs, item) {
+//   var found = false;
+//   for (var x of xs) {
+//     if (found) return x;
+//     if (x == item) found = true;
+//   }
+//   return undefined;
+// }
